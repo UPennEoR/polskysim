added several instrument setup files, fixed day counter and added loop over sky realizations for final day averaging

IonRIME/instrument_setup.py
@@ -3,6 +3,7 @@ import healpy as hp
 import ionRIME_funcs as irf
 import os
 import sys
+import errno
 
 import hera_NicCST_instrument_setup as hnis
 import hera_CTP_instrument_setup as hcis
@@ -11,10 +12,11 @@ import hera_nicsinuous_instrument_setup as hnsis
 import hera_hfss_instrument_setup as hhis
 import paper_hfss_instrument_setup as phis
 import legacy_cst_instrument_setup as lcis
+import sim_group_test0_instrument_setup as sgtest0
 
 
 class InstrumentConstructor(object):
-    def __init__(self, parameters):
+    def __init__(self, parameters, antenna='a'):
         for key in parameters:
             setattr(self, key, parameters[key])
 
@@ -38,6 +40,14 @@ class InstrumentConstructor(object):
             'm': self.m,
             'z0_cza': self.z0_cza})
 
+        if antenna == 'b':
+            if self.instrument_b != 'same':
+                self.antenna_model = self.instrument_b
+            else:
+                self.antenna_model = self.instrument
+        else:
+            self.antenna_model = self.instrument
+
         nu0 = str(int(self.nu_axis[0] / 1e6))
         nuf = str(int(self.nu_axis[-1] / 1e6))
         self.fname = "ijones" + "band_" + nu0 + "-" + nuf + "mhz_nfreq" + str(self.nfreq)+ "_nside" + str(self.nside) + ".npy"
@@ -51,10 +61,11 @@ class InstrumentConstructor(object):
             'paper_hfss': 'jones_save/PAPER_HFSS/',
             'paper_feko': 'jones_save/PAPER/',
             'hera_legacy': 'jones_save/',
-            'analytic_dipole': 'analytic_dipole/'
+            'analytic_dipole': 'analytic_dipole/',
+            'sim_group_test0': 'jones_save/sim_group_test0/'
         }
 
-        self.relative_path = self.InstrumentDirectories[self.instrument] + self.fname
+        self.relative_path = self.InstrumentDirectories[self.antenna_model] + self.fname
 
         self.InstrumentGenerators = {
             'hera_NicCST': self.hera_NicCST,
@@ -66,14 +77,21 @@ class InstrumentConstructor(object):
             'paper_feko': self.paper_feko,
             'hera_legacy': self.hera_legacy,
             'analytic_dipole': self.analytic_dipole,
+            'sim_group_test0': self.sim_group_test0,
         }
+
+        try:
+            os.makedirs(self.InstrumentDirectories[self.antenna_model])
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
 
         if os.path.exists(self.relative_path) is True:
             self.iJonesModel = np.load(self.relative_path)
             print "Restored Jones model"
         else:
-            self.iJonesModel = self.InstrumentGenerators[self.instrument](parameters)
-            if self.instrument != 'analytic_dipole':
+            self.iJonesModel = self.InstrumentGenerators[self.antenna_model](parameters)
+            if self.antenna_model != 'analytic_dipole':
                 np.save(self.relative_path, self.iJonesModel)
 
 
@@ -84,7 +102,7 @@ class InstrumentConstructor(object):
         nuf = str(int(self.nu_axis[-1] / 1e6))
         fname = "norms_ijones" + "band_" + nu0 + "-" + nuf + "mhz_nfreq" + str(self.nfreq)+ "_nside" + str(self.nside) + '.npz'
 
-        save_path = self.InstrumentDirectories[self.instrument] + fname
+        save_path = self.InstrumentDirectories[self.antenna_model] + fname
         np.savez(save_path, solid_angle=solid_angle, peak_norms=peak_norms)
         return ijones
 
@@ -95,7 +113,7 @@ class InstrumentConstructor(object):
         nuf = str(int(self.nu_axis[-1] / 1e6))
         fname = "norms_ijones" + "band_" + nu0 + "-" + nuf + "mhz_nfreq" + str(self.nfreq)+ "_nside" + str(self.nside) + '.npz'
 
-        save_path = self.InstrumentDirectories[self.instrument] + fname
+        save_path = self.InstrumentDirectories[self.antenna_model] + fname
         np.savez(save_path, solid_angle=solid_angle, peak_norms=peak_norms)
         return ijones
 
@@ -106,7 +124,7 @@ class InstrumentConstructor(object):
         nuf = str(int(self.nu_axis[-1] / 1e6))
         fname = "norms_ijones" + "band_" + nu0 + "-" + nuf + "mhz_nfreq" + str(self.nfreq)+ "_nside" + str(self.nside) + '.npz'
 
-        save_path = self.InstrumentDirectories[self.instrument] + fname
+        save_path = self.InstrumentDirectories[self.antenna_model] + fname
         np.savez(save_path, solid_angle=solid_angle, peak_norms=peak_norms)
         return ijones
 
@@ -117,7 +135,7 @@ class InstrumentConstructor(object):
         nuf = str(int(self.nu_axis[-1] / 1e6))
         fname = "norms_ijones" + "band_" + nu0 + "-" + nuf + "mhz_nfreq" + str(self.nfreq)+ "_nside" + str(self.nside) + '.npz'
 
-        save_path = self.InstrumentDirectories[self.instrument] + fname
+        save_path = self.InstrumentDirectories[self.antenna_model] + fname
         np.savez(save_path, solid_angle=solid_angle, peak_norms=peak_norms)
         return ijones
 
@@ -137,3 +155,6 @@ class InstrumentConstructor(object):
 
     def analytic_dipole(self, parameters):
         return irf.analytic_dipole_setup(self.nside, self.nfreq,sigma=self.dipole_hpbw, z0_cza=self.z0_cza)
+
+    def sim_group_test0(self, parameters):
+        return sgtest0.make_ijones_spectrum(parameters)

IonRIME/ionRIME.py
@@ -4,6 +4,7 @@ import sys, os
 import yaml
 from shutil import copyfile, rmtree
 import git
+import h5py
 
 
 if __name__ == "__main__":
@@ -51,12 +52,32 @@ if __name__ == "__main__":
     if P['circular_pol'] is False:
         print "No circular polarization! Booooo."
 
-    VS = VisibilitySimulation(P)
-    VS.run()
+    if P['MC_sky'] is False:
+        VS = VisibilitySimulation(P)
+        VS.run()
 
-    repo = git.Repo(search_parent_directories=True)
-    sha = repo.head.object.hexsha
+        repo = git.Repo(search_parent_directories=True)
+        sha = repo.head.object.hexsha
 
-    out_name = "visibility.npz"
+        out_name = "visibility.npz"
 
-    np.savez(out_dir + out_name, Vis=VS.Vis, parameters=P, git_hash=sha)
+        np.savez(out_dir + out_name, Vis=VS.Vis, parameters=P, git_hash=sha)
+
+    if P['MC_sky'] is True:
+        N_skies = P['MC_sky_realizations']
+
+        out_name = 'visibilities.h5'
+
+        h5f = h5py.File(out_dir + out_name, 'w')
+
+        for n in range(N_skies):
+            VS = VisibilitySimulation(P)
+            VS.run()
+            h5f.create_dataset(str(n), data=VS.Vis)
+
+        h5f.close()
+
+        repo = git.Repo(search_parent_directories=True)
+        sha = repo.head.object.hexsha
+
+        np.savez(out_dir + 'meta_data.npz', parameters=P, git_hash=sha)

IonRIME/sky_setup.py
@@ -61,7 +61,9 @@ class SkyConstructor(object):
             'point_skyE': self.point_skyE,
             'point_skyF': self.point_skyF,
             'point_skyG': self.point_skyG,
-            'point_skyH': self.point_skyH
+            'point_skyH': self.point_skyH,
+            'sim_group_test0': self.sim_group_test0,
+            'get_cora_polsky': self.get_cora_polsky,
         }
 
         return skyGenerators
@@ -515,7 +517,7 @@ class SkyConstructor(object):
 
         gal = galaxy.ConstrainedGalaxy()
         gal.nside = self.nside
-        gal.frequencies = self.nu_axis
+        gal.frequencies = self.nu_axis * 1e-6 # MHz - self.nu_axis is in Hz.
 
         stokes_cubes = gal.getpolsky()
         I,Q,U,V = [stokes_cubes[:,i,:] for i in range(4)]
@@ -547,4 +549,30 @@ class SkyConstructor(object):
     def unpol_GSM2008(self):
         I = self.GSM2008()
         Q,U,V = [np.zeros_like(I) for k in range(3)]
+        return I,Q,U,V
+
+    def sim_group_test0(self):
+        x_c = np.array([1.,0,0]) # unit vectors to be transformed by astropy
+        y_c = np.array([0,1.,0])
+        z_c = np.array([0,0,1.])
+
+        # The GSM is given in galactic coordinates. We will rotate it to J2000 equatorial coordinates.
+        axes_icrs = coord.SkyCoord(x=x_c, y=y_c, z=z_c, frame='icrs', representation='cartesian')
+        axes_gal = axes_icrs.transform_to('galactic')
+        axes_gal.representation = 'cartesian'
+
+        R = np.array(axes_gal.cartesian.xyz) # The 3D rotation matrix that defines the coordinate transformation.
+
+        I = np.zeros((self.nfreq, self.npix))
+
+        gsm = pygsm.GlobalSkyModel(freq_unit='MHz', interpolation='cubic')
+        I_150 = gsm.generate(150.)
+        rI_150 = irf.rotate_healpix_mindex(I_150, R)
+        urI_150 = irf.harmonic_ud_grade(rI_150, self.nside)
+
+        for i,nu in enumerate(self.nu_axis):
+            I[i,:] = urI_150
+
+        Q,U,V = [np.zeros_like(I) for k in range(3)]
+
         return I,Q,U,V

IonRIME/visibility_simulation.py
@@ -55,6 +55,12 @@ class VisibilitySimulation(object):
         if attribute_test is None:
             self.z0_cza = np.radians(120.7215) # latitude of HERA/PAPER
             parameters.update({'z0_cza':self.z0_cza})
+        # older input parameters files will not have an instrument_b setting
+        attribute_test = getattr(self, 'instrument_b',None)
+        if attribute_test is None:
+            self.instrument_b = 'same'
+            parameters.update({'instrument_b':self.instrument_b})
+
 
         ## Sky
         ## sky.shape = (self.nfreq, self.npix, 2,2)
@@ -78,10 +84,11 @@ class VisibilitySimulation(object):
         tmark0 = time.time()
 
         print "Setting up instrument."
-        instrument_init = InstrumentConstructor(parameters)
-        self.ijones = instrument_init.iJonesModel
+        instrument_a_init = InstrumentConstructor(parameters, antenna='a')
+        self.ijones = instrument_a_init.iJonesModel
 
-        self.ijonesH = np.transpose(self.ijones.conj(),(0,1,3,2))
+        instrument_b_init = InstrumentConstructor(parameters, antenna='b')
+        self.ijonesH = np.transpose(instrument_b_init.iJonesModel.conj(),(0,1,3,2))
 
         tmark_inst = time.time()
         print "Completed instrument setup in " + str(tmark_inst - tmark0)
@@ -99,9 +106,12 @@ class VisibilitySimulation(object):
 
         if self.ionosphere == 'none':
             self.ndays = 1
-
-        self.Vis = np.zeros(self.ndays * self.ntime * self.nfreq * 2 * 2, dtype='complex128')
-        self.Vis = self.Vis.reshape(self.ndays, self.ntime, self.nfreq, 2, 2)
+        if self.final_day_average is False:
+            self.Vis = np.zeros(self.ndays * self.ntime * self.nfreq * 2 * 2, dtype='complex128')
+            self.Vis = self.Vis.reshape(self.ndays, self.ntime, self.nfreq, 2, 2)
+        else:
+            self.Vis = np.zeros(1 * self.ntime * self.nfreq * 2 * 2, dtype='complex128')
+            self.Vis = self.Vis.reshape(1, self.ntime, self.nfreq, 2, 2)
 
         self.RMs = []
 
@@ -163,7 +173,12 @@ class VisibilitySimulation(object):
                 for ti in range(self.ntime):
                     self.UT_times[di].append(di)
 
-        for d in range(self.ndays):
+        if self.final_day_average is True:
+            Nd_use = 1
+        else:
+            Nd_use = self.ndays
+
+        for d in range(Nd_use):
             print "d is " + str(d)
             for t in range(self.ntime):
                 if self.point_source_sim is True: